9a98a887e3f68ee8483aa7d66ee5a2ed871293ac
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-07-21 19:31:05 +0100

    adjusted for smaller screen sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer () {
     return (
-        <footer className="flex flex-col justify-center items-center text-center bg-green h-1/6 text-black tracking-wider text-xl">
+        <footer className="px-4 flex flex-col justify-center items-center text-center bg-green h-1/6 text-black tracking-wider text-xl">
             <p>
                 made with &hearts; by <a className="underline" href="https://chukwudumebi.me" target="blank">Chukwudumebi</a> &copy; 2021</p>
         </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import girl from '../images/girl.png'
 
 export default function Header () {
     return (
-        <header className="relative bg-gradient-to-l from-purple via-blush to-purple h-5/6 flex flex-col justify-between items-center text-xl text-snow tracking-widest">
-            <div className="md:mt-24 flex flex-row items-center">
-                <div className="flex flex-col justify-between items-center -mr-48 z-30">
-                   <p className="text-stroke-4 text-stroke-black text-9xl">Sk8r</p>
+        <header className="px-4 relative bg-gradient-to-l from-purple via-blush to-purple h-5/6 flex flex-col justify-between items-center text-xl text-snow tracking-widest">
+            <div className="mt-60 lg:mt-28 flex flex-row items-center">
+                <div className="flex flex-col justify-between items-center lg:-mr-48 z-30">
+                   <p className="text-stroke-2 lg:text-stroke-4 text-stroke-black text-5xl lg:text-9xl">Sk8r</p>
                     <a href="/">
-                        <button className="flex flex-row items-center border-2 w-min px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
+                        <button className="flex flex-row items-center border-2 w-min px-2 lg:px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
                             </svg>
@@ -15,10 +15,10 @@ export default function Header () {
                         </button>
                     </a>
                 </div>
-                <img src={girl} alt="skating-girl" className=" z-20" />
-                <div className="flex flex-col justify-between items-center -ml-48 z-30">
-                    <p className="text-stroke-4 text-stroke-black text-9xl">Girl</p>
-                    <button className="flex flex-row items-center border-2 w-min px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
+                <img src={girl} alt="skating-girl" className="-ml-28 lg:-ml-0 z-20" />
+                <div className="flex flex-col justify-between items-center -ml-28 lg:-ml-48 z-30">
+                    <p className="text-stroke-2 lg:text-stroke-4 text-stroke-black text-5xl lg:text-9xl">Girl</p>
+                    <button className="flex flex-row items-center border-2 w-min px-2 lg:px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
                         settings
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-4" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
