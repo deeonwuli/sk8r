cf3244230cd7f7c7bc391c97aef4bb5ca23e0145
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-07-21 19:46:29 +0100

    fixes for heroku deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,12 @@ export default function Header () {
             <div className="mt-60 lg:mt-28 flex flex-row items-center">
                 <div className="flex flex-col justify-between items-center lg:-mr-48 z-30">
                    <p className="text-stroke-2 lg:text-stroke-4 text-stroke-black text-5xl lg:text-9xl">Sk8r</p>
-                    <a href="/">
-                        <button className="flex flex-row items-center border-2 w-min px-2 lg:px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                            </svg>
-                            play
-                        </button>
-                    </a>
+                    <button className="flex flex-row items-center border-2 w-min px-2 lg:px-5 py-2 rounded-3xl bg-blue border-blue shadow-xl hover:bg-blue-green hover:-translate-y-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+                        </svg>
+                        play
+                    </button>
                 </div>
                 <img src={girl} alt="skating-girl" className="-ml-28 lg:-ml-0 z-20" />
                 <div className="flex flex-col justify-between items-center -ml-28 lg:-ml-48 z-30">
