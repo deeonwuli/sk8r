190b8991b29fdb9c061c5e321d69501f22467ad8
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-11-11 22:12:00 +0100

    basic gameplay implementation

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,12 +1,98 @@
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 
 export default function Play() {
+  const [shirt, setShirt] = useState(false);
+  const [bottoms, setBottoms] = useState(false);
+  const [gear, setGear] = useState(false);
+  const [shoes, setShoes] = useState(false);
+
+  const toggleShirt = () => {
+    setShirt(!shirt);
+  };
+
+  const toggleBottoms = () => {
+    setBottoms(!bottoms);
+  };
+
+  const toggleGear = () => {
+    setGear(!gear);
+  };
+
+  const toggleShoes = () => {
+    setShoes(!shoes);
+  };
+
+  const Shirt = () => {
+    return (
+      <div
+        className="h-40 w-40 p-5 z-20 absolute"
+        style={{ backgroundColor: "red" }}
+      ></div>
+    );
+  };
+
+  const Shoes = () => {
+    return (
+      <div
+        className="h-40 w-40 p-5 z-20 absolute"
+        style={{ backgroundColor: "yellow", top: "85%" }}
+      ></div>
+    );
+  };
+
+  const Bottoms = () => {
+    return (
+      <div
+        className="h-40 w-40 p-5 z-20 absolute"
+        style={{ backgroundColor: "green", top: "60%" }}
+      ></div>
+    );
+  };
+
+  const Gear = () => {
+    return (
+      <div
+        className="h-40 w-40 p-5 z-20 absolute top-12"
+        style={{ backgroundColor: "blue", bottom: "70%" }}
+      ></div>
+    );
+  };
+
   return (
-    <div className="h-screen w-screen">
-      <div className="w-full h-5/6 bg-gradient-to-l from-purple via-blush to-purple relative flex flex-col justify-center items-center">
-        <div className="container">
-          <img src="/images/body.png" alt="fashion girl" style={{height: "32rem"}} />
+    <div className="h-screen w-screen bg-gradient-to-l from-purple via-blush to-purple">
+      <div className="h-5/6 relative flex justify-center items-center">
+        <div
+          className="absolute"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+            rowGap: "10rem",
+            columnGap: "20rem",
+          }}
+        >
+          <button className="bg-white p-5 rounded-full" onClick={toggleShirt}>
+            shirt
+          </button>
+          <button className="bg-white p-5 rounded-full" onClick={toggleBottoms}>
+            bottoms
+          </button>
+          <button className="bg-white p-5 rounded-full" onClick={toggleShoes}>
+            shoes
+          </button>
+          <button className="bg-white p-5 rounded-full" onClick={toggleGear}>
+            gear
+          </button>
         </div>
+        <img
+          src="/images/body.png"
+          alt="fashion girl"
+          style={{ height: "32rem" }}
+        />
+        {shirt ? <Shirt /> : null}
+        {bottoms ? <Bottoms /> : null}
+        {shoes ? <Shoes /> : null}
+        {gear ? <Gear /> : null}
       </div>
       <Footer />
     </div>
