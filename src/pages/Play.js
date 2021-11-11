768391916ac16bb6995fef505340bb83af45b669
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-11-11 20:46:03 +0100

    new game app layout

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -2,8 +2,13 @@ import Footer from "../components/Footer";
 
 export default function Play() {
   return (
-    <>
+    <div className="h-screen w-screen">
+      <div className="w-full h-5/6 bg-gradient-to-l from-purple via-blush to-purple relative flex flex-col justify-center items-center">
+        <div className="container">
+          <img src="/images/body.png" alt="fashion girl" style={{height: "32rem"}} />
+        </div>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
